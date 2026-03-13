764b40ade450ecfa12bc491f75b3b19755f0f98c
Igorshk10 <igorshk271@gmail.com>
2026-03-13 13:50:29 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://fan-fiction-frontend.onrender.com']
     credentials: true,
   });
   app.useGlobalPipes(
